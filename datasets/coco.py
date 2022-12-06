ded2260bdd05875839cbd933cd740f3388ec2777
ijliu <ijliu@outlook.com>
2022-12-06 15:35:05 +0800

    Updated

---
+++ datasets/coco.py
@@ -0,0 +1,20 @@
+#
+# coco.py
+#
+
+class COCO:
+	"""
+	读取/可视化 COCO 格式的标注文件
+	"""
+	def __init__(self,):
+		pass
+
+	def __load_images(self,):
+		pass
+	
+	def __load_jsons(self,):
+		pass
+	
+	def vis(self,):
+		# 可视化
+		pass
